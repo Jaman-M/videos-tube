2e60ecaf14be7ee6b5831736b25d6f4cfeef1097
Jaman-M <jaman.manik1@gmail.comm>
2025-02-26 06:25:57 +0800

    Load Videos and display cards to the UI

--- scripts/videos.js
+++ scripts/videos.js
@@ -11,6 +11,61 @@ const loadCategories = () => {
         .catch((error) => console.log(error))
 }
 
+// load videos 
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error))
+};
+
+// this is the object copied
+const cardDemo = {
+    "category_id": "1001",
+    "video_id": "aaag",
+    "thumbnail": "https://i.ibb.co/DRxB1Wm/sunris.jpg",
+    "title": "Sunrise Reverie",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/yQFJ42h/ava.jpg",
+            "profile_name": "Ava Johnson",
+            "verified": false
+        }
+    ],
+    "others": {
+        "views": "1.1K",
+        "posted_date": "16950"
+    },
+    "description": "'Sunrise Reverie' by Ava Johnson takes listeners on a serene journey through tranquil melodies and soft harmonies. With 1.1K views, this track is perfect for morning relaxation or an evening wind-down. Ava's heartfelt lyrics and soothing voice create a sense of peace, making it a go-to for fans seeking calm and inspiration in their musical choices."
+}
+
+const displayVideos = (videos) => {
+    // console.log(videos);
+    const videoContainer = document.getElementById('videos');
+    videos.forEach((video) => {
+        console.log(video);
+        // card making
+        const card = document.createElement('div');
+        card.classList = 'card card-compact'
+        card.innerHTML =
+            `
+        <figure>
+    <img
+      src= ${video.thumbnail} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `
+
+        videoContainer.append(card);
+    })
+}
+
 //create displayCategorier
 const displayCategories = (categories) => {
 
@@ -33,4 +88,5 @@ const displayCategories = (categories) => {
 }
 
 
-loadCategories()
+loadCategories();
+loadVideos();
