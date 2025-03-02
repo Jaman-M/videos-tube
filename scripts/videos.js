097dd8bfaeaa95d7aac7dd2a8d7b2cee8738a8db
Jaman-M <jaman.manik1@gmail.comm>
2025-03-02 09:00:16 +0800

    show the time only who have time given as posted_date and nothing will show in other cases

--- scripts/videos.js
+++ scripts/videos.js
@@ -49,10 +49,12 @@ const displayVideos = (videos) => {
         card.classList = 'card card-compact'
         card.innerHTML =
             `
-        <figure class='h-[200px]'>
+        <figure class='h-[200px] relative'>
     <img
     class='h-full w-full object-cover'
       src= ${video.thumbnail} />
+      ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 rounded p-1 bg-black text-white">${video.others.posted_date}</span>`}
+      
   </figure>
   <div class="px-0 py-2 gap-2 flex">
     <div>
@@ -63,7 +65,7 @@ const displayVideos = (videos) => {
         <div class="flex items-center gap-2">
             <p class=text-gray-400>${video.authors[0].profile_name}</p>
 
-            
+
             ${video.authors[0].verified == true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ""}
         </div>
         
